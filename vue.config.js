9b6ef26906fcfa3afc191b915965814b3e84e3c6
wangfan <i.wangfan@foxmail.com>
2018-08-16 17:42:12 +0800

    add viewport layout

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  baseUrl: '/'
+  baseUrl: '/',
+  devServer: {
+    port: 3174
+  },
+  pages: {
+    account: {
+      entry: 'src/main.ts',
+      template: 'public/index.1.html',
+      filename: 'account.html',
+      title: 'account'
+    },
+    index: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'index'
+    }
+  }
 }
