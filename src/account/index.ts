1342ab1aec59a18c83927ccaef1b0b5ee14ca6f2
iwfan <i.wangfan@foxmail.com>
2018-10-10 23:22:39 +0800

    initial commit

--- src/account/index.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Account from './Account.vue'
-import 'normalize.css'
-import '@/assets/css/reset.styl'
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(Account)
-}).$mount('#app')
